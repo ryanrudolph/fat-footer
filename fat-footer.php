766c8f7a7b1c875e712472f24aef2320456daec2
Ryan Rudolph <ryan@ryanrudolph.com>
2017-07-09 12:51:21 -0400

    setting up settings

--- fat-footer.php
+++ fat-footer.php
@@ -2,12 +2,19 @@
 /**
  * Plugin Name: Fat Footer
  * Plugin URI: http://getphound.com
- * Description: This plugin adds the fat footer to the bottom of the website.
+ * Description: This plugin adds a fat footer to the bottom of the website.
  * Version: 0.0.1
  * Author: Ryan Rudolph
  * Author URI: http://ryanrudolph.com
  * License: GPL2
  */
+
+// Enqueue styles
+add_action( 'wp_enqueue_styles', 'my_enqueued_styles' );
+
+function my_enqueued_styles() {
+	wp_enqueue_style( 'my-style', plugin_dir_url( __FILE__ ) . '/style.css' );
+}
 
 // Register widget area
 genesis_register_sidebar( array(
@@ -20,4 +27,20 @@ add_action( 'genesis_before_footer', 'gp_fat_footer' );
 
 function gp_fat_footer() {
 
+    genesis_widget_area( 'fat-footer', array(
+		'before' => '<div class="fat-footer widget-area">',
+		'after'  => '</div>',
+    ) );
+
+}
+
+// Add settings menu
+add_action('admin_menu', 'my_plugin_menu');
+
+function my_plugin_menu() {
+	add_menu_page('Fat Footer Settings', 'Plugin Settings', 'administrator', 'my-plugin-settings', 'my_plugin_settings_page', 'dashicons-admin-generic');
+}
+
+function my_plugin_settings_page() {
+  // 
 }
