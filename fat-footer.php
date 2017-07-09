d5358a65d3be9ba7cff4ffa70362e091708384b2
Ryan Rudolph <ryan@ryanrudolph.com>
2017-07-09 13:08:33 -0400

    setup initial settings

--- fat-footer.php
+++ fat-footer.php
@@ -20,9 +20,46 @@ function my_enqueued_styles() {
 add_action('admin_menu', 'my_plugin_menu');
 
 function my_plugin_menu() {
-	add_menu_page('Fat Footer Settings', 'Plugin Settings', 'administrator', 'my-plugin-settings', 'my_plugin_settings_page', 'dashicons-admin-generic');
+	add_menu_page('Fat Footer Settings', 'Fat Footer Settings', 'administrator', 'my-plugin-settings', 'my_plugin_settings_page', 'dashicons-admin-generic');
 }
 
-function my_plugin_settings_page() {
-  // 
+function my_plugin_settings_page() { ?>
+	
+<div class="wrap">
+<h2>Fat Footer Settings</h2>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'my-plugin-settings-group' ); ?>
+    <?php do_settings_sections( 'my-plugin-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Business Address</th>
+        <td><input type="text-area" name="business_address" value="<?php echo esc_attr( get_option('business_address') ); ?>" /></td>
+        </tr>
+         
+        <tr valign="top">
+        <th scope="row">Business Email</th>
+        <td><input type="text" name="business_phone" value="<?php echo esc_attr( get_option('business_phone') ); ?>" /></td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">Business Phone</th>
+        <td><input type="text" name="business_email" value="<?php echo esc_attr( get_option('business_email') ); ?>" /></td>
+        </tr>
+    </table>
+    
+    <?php submit_button(); ?>
+
+</form>
+</div>
+
+
+<?php }
+
+add_action( 'admin_init', 'my_plugin_settings' );
+
+function my_plugin_settings() {
+	register_setting( 'my-plugin-settings-group', 'business_address' );
+	register_setting( 'my-plugin-settings-group', 'business_phone' );
+	register_setting( 'my-plugin-settings-group', 'business_email' );
 }
