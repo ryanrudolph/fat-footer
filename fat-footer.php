2b428a13449dc23a3c546c3a8cfb8d7703c99294
Ryan Rudolph <ryan@ryanrudolph.com>
2017-07-09 12:51:49 -0400

    removed widget registration

--- fat-footer.php
+++ fat-footer.php
@@ -16,24 +16,6 @@ function my_enqueued_styles() {
 	wp_enqueue_style( 'my-style', plugin_dir_url( __FILE__ ) . '/style.css' );
 }
 
-// Register widget area
-genesis_register_sidebar( array(
-	'id' => 'fat-footer',
-	'name' => __( 'Fat Footer', 'genesis' ),
-	'description' => __( 'This is the widget area is intended to be used with the Fat Footer widget.', 'childtheme' ),
-) );
-
-add_action( 'genesis_before_footer', 'gp_fat_footer' );
-
-function gp_fat_footer() {
-
-    genesis_widget_area( 'fat-footer', array(
-		'before' => '<div class="fat-footer widget-area">',
-		'after'  => '</div>',
-    ) );
-
-}
-
 // Add settings menu
 add_action('admin_menu', 'my_plugin_menu');
 
